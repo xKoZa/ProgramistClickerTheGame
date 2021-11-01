b990b9e1b144892b0348764949426c91a0e24f0c
xKoZa <filip.j.pietrzak@gmail.com>
2021-11-01 16:41:01 +0100

    First build of game
    Added a loooot of things

---
+++ Assets/Scripts/ChaningTabs.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaningTabs : MonoBehaviour
+{
+    /*public List<Stages> stagesScenes;
+    public void sceny(Stages button){
+        if (stagesScenes == null){
+            stagesScenes = new List<Stages>();
+        }
+        stagesScenes.Add(button);
+    }
+    public void OnSceneEnter(Stages button){
+
+    }
+    public void OnSceneExit(Stages button){
+
+    }
+    public void OnSceneSelected(Stages button){
+        
+    }*/
+    
+}
